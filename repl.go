c457a0bf8830b3e89409d66a2d3fd76b8f129fca
Stanley John <stanleyjohn98@gmail.com>
2026-04-11 17:18:12 +0530

    Organizing code to accept wider commands

---
+++ repl.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(" >")
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		command := cleaned[0]
+
+		availableCommands := getCommands()
+		command, ok := availableCommands[command]
+		if !ok {
+			fmt.Println("invalid command")
+			continue
+		}
+		command.callback()
+
+		switch command {
+		case "help":
+			fmt.Println("Welcome to the Pokedex help menu!")
+			fmt.Println("Here are your available commands: ")
+			fmt.Println(" - help")
+			fmt.Println(" - exit")
+			fmt.Println("")
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("invalid command")
+		}
+
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Prints the help menu",
+			callback:    callbackHelp,
+		},
+		"exit": {
+			name:        "exist",
+			description: "Turns off the Pokedex",
+			callback:    callbackExit,
+		},
+	}
+}
+
+func cleanInput(str string) []string {
+	lowered := strings.ToLower(str)
+	words := strings.Fields(lowered)
+	return words
+}
